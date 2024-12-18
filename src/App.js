42e298b4a2ea753fbee3fff329bf0943ef7e9690
Matheus Hygino <matheusghygino@gmail.com>
2024-12-17 22:56:54 -0300

    chamando botão

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from './components/Button';
+
 function App() {
   return (
     <div className="App">
-      Olá mundo!
+      <h1>Olá React</h1>
+      <Button tittle="Entrar"/>
     </div>
   );
 }
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,7 @@
-export default function Button({tittle}) {
-  return (
+export default function Button(props) {
+    const {tittle} = props;
+    
+    return (
     <button>{tittle}</button>
   )
 }
